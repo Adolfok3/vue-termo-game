a850e0498b19ce6f381a61fb0c375da188fc71db
Luiz Adolfo <luiz-adolfo@hotmail.com>
2022-07-11 12:40:33 -0300

    adicionado links de contato

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,20 +1,45 @@
 <template>
   <div class="title">
+    <img height="40" class="github" :src="require('../assets/github.png')" @click="goToVueTermoGameGithub()"/>
     <span class="title-text">Vue Termo Game</span>
+    <span class="adolfok3" @click="goToAdolfok3Github()">@Adolfok3</span>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    goToAdolfok3Github() {
+      window.open('https://github.com/Adolfok3', '_blank')
+    },
+    goToVueTermoGameGithub() {
+      window.open('https://github.com/Adolfok3/vue-termo-game', '_blank')
+    },
+  },
+};
+</script>
 
 <style scoped>
 .title {
   width: 100%;
   display: flex;
   align-items: center;
-  justify-content: center;
-  padding-top: 1vmin;
+  justify-content: space-between;
 }
 
 .title-text {
   color: white;
   font-size: 2.7vmin;
+}
+
+.adolfok3 {
+  color: white;
+  font-size: 1.5vmin;
+  cursor: pointer;
+}
+
+.github {
+  color: white;
+  cursor: pointer;
 }
 </style>
